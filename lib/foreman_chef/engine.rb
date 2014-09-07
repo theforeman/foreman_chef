6e9617d8b7baf70200c0b8b6446b2e626fab7fd6
Marek Hulan <mhulan@redhat.com>
2014-09-07 11:07:35 +0200

    Fix autoreloadign issues

--- lib/foreman_chef/engine.rb
+++ lib/foreman_chef/engine.rb
@@ -16,28 +16,20 @@ module ForemanChef
     initializer 'foreman_chef.register_plugin', :after => :finisher_hook do |app|
       Foreman::Plugin.register :foreman_chef do
         requires_foreman '>= 1.4'
-      end if Foreman::Plugin rescue(false)
+      end
     end
 
     initializer 'foreman_chef.chef_proxy_form' do |app|
       ActionView::Base.send :include, ChefProxyForm
     end
 
-    initializer 'foreman_chef.host_extensions' do |app|
-      Host::Managed.send :include, ForemanChef::HostExtensions
-    end
-
-    initializer 'foreman_chef.hostgroup_extensions' do |app|
-      Hostgroup.send :include, ForemanChef::HostgroupExtensions
-    end
-
-    initializer 'foreman_chef.chef_proxy_association' do |app|
-      Host::Managed.send :include, ChefProxyAssociation
-      Hostgroup.send :include, ChefProxyAssociation
-    end
-
     #Include extensions to models in this config.to_prepare block
     config.to_prepare do
+      ::Host::Managed.send :include, ForemanChef::HostExtensions
+      ::Hostgroup.send :include, ForemanChef::HostgroupExtensions
+      ::Host::Managed.send :include, ChefProxyAssociation
+      ::Hostgroup.send :include, ChefProxyAssociation
+      ::SmartProxy.send :include, SmartProxyExtensions
       ::FactImporter.register_fact_importer(:foreman_chef, ForemanChef::FactImporter)
       ::Host::Base.send :include, ForemanChef::Concerns::HostActionSubject
     end
