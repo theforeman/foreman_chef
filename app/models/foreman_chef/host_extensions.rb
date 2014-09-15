28b1f9cbfc81bc8b1d852cff6b83f5a12aebed7d
Marek Hulan <mhulan@redhat.com>
2014-09-15 14:55:31 +0200

    Fix deprecation warnings

--- app/models/foreman_chef/host_extensions.rb
+++ app/models/foreman_chef/host_extensions.rb
@@ -6,12 +6,10 @@ module ForemanChef
       alias_method_chain :set_hostgroup_defaults, :chef_proxy
     end
 
-    module InstanceMethods
-      def set_hostgroup_defaults_with_chef_proxy
-        set_hostgroup_defaults_without_chef_proxy
-        return unless hostgroup
-        assign_hostgroup_attributes(['chef_proxy_id'])
-      end
+    def set_hostgroup_defaults_with_chef_proxy
+      set_hostgroup_defaults_without_chef_proxy
+      return unless hostgroup
+      assign_hostgroup_attributes(['chef_proxy_id'])
     end
 
   end
--- app/models/foreman_chef/smart_proxy_extensions.rb
+++ app/models/foreman_chef/smart_proxy_extensions.rb
@@ -3,37 +3,35 @@ module ForemanChef
     extend ActiveSupport::Concern
 
     # create or overwrite instance methods...
-    module InstanceMethods
-      def show_node(fqdn)
-        reply = ProxyAPI::ForemanChef::ChefProxy.new(:url => url).show_node(fqdn)
-        JSON.parse(reply)
-      rescue RestClient::ResourceNotFound
-        logger.debug "Node '#{fqdn}' not found"
-        return false
-      end
+    def show_node(fqdn)
+      reply = ProxyAPI::ForemanChef::ChefProxy.new(:url => url).show_node(fqdn)
+      JSON.parse(reply)
+    rescue RestClient::ResourceNotFound
+      logger.debug "Node '#{fqdn}' not found"
+      return false
+    end
 
-      def delete_node(fqdn)
-        begin
-          reply = ProxyAPI::ForemanChef::ChefProxy.new(:url => url).delete_node(fqdn)
-          JSON.parse(reply)
+    def delete_node(fqdn)
+      begin
+        reply = ProxyAPI::ForemanChef::ChefProxy.new(:url => url).delete_node(fqdn)
+        JSON.parse(reply)
+      #rescue RestClient::
+      end
+    end
+
+    def show_client(fqdn)
+      reply = ProxyAPI::ForemanChef::ChefProxy.new(:url => url).show_client(fqdn)
+      JSON.parse(reply)
+    rescue RestClient::ResourceNotFound
+      logger.debug "Client '#{fqdn}' not found"
+      return false
+    end
+
+    def delete_client(fqdn)
+      begin
+        reply = ProxyAPI::ForemanChef::ChefProxy.new(:url => url).delete_client(fqdn)
+        JSON.parse(reply)
         #rescue RestClient::
-        end
-      end
-
-      def show_client(fqdn)
-        reply = ProxyAPI::ForemanChef::ChefProxy.new(:url => url).show_client(fqdn)
-        JSON.parse(reply)
-      rescue RestClient::ResourceNotFound
-        logger.debug "Client '#{fqdn}' not found"
-        return false
-      end
-
-      def delete_client(fqdn)
-        begin
-          reply = ProxyAPI::ForemanChef::ChefProxy.new(:url => url).delete_client(fqdn)
-          JSON.parse(reply)
-          #rescue RestClient::
-        end
       end
     end
   end
