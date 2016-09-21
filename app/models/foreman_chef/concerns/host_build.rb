8dc50798b9b93322b5dbee2be70d4b7fa7a86420
Marek Hulan <mhulan@redhat.com>
2016-09-21 17:01:42 +0200

    Fixes #16641 - fix after build hook to only delete client

--- app/models/foreman_chef/concerns/host_build.rb
+++ app/models/foreman_chef/concerns/host_build.rb
@@ -6,7 +6,18 @@ module ForemanChef
         include ForemanTasks::Concerns::ActionSubject
         include ForemanTasks::Concerns::ActionTriggering
 
-        after_build :plan_destroy_action
+        after_build do |host|
+          ::ForemanTasks.sync_task ::Actions::ForemanChef::Client::Destroy, host.name, host.chef_proxy
+          # private key is no longer valid
+          host.chef_private_key = nil
+
+          if Setting.validation_bootstrap_method
+            new_client = ::ForemanTasks.sync_task ::Actions::ForemanChef::Client::Create, host.name, host.chef_proxy
+            host.chef_private_key = new_client.output[:private_key]
+          end
+
+          host.disable_dynflow_hooks { |h| h.save! }
+        end
       end
 
     end
