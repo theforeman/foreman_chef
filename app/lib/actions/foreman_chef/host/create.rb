d7d007b77be2ae61a3e954ff2b51372346992edd
Marek Hulan <mhulan@redhat.com>
2017-12-22 17:01:34 +0100

    Refs #19620 - add explicit namespace

--- app/lib/actions/foreman_chef/host/create.rb
+++ app/lib/actions/foreman_chef/host/create.rb
@@ -26,7 +26,7 @@ module Actions
             end
           end
         rescue => e
-          Foreman::Logging.exception("Unable to communicate with Chef proxy", e)
+          ::Foreman::Logging.exception("Unable to communicate with Chef proxy", e)
           raise ::ForemanChef::ProxyException.new(N_('Unable to communicate with Chef proxy, %s' % e.message))
         end
 
