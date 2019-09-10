b18ab441767fe3990719fd5836a330331641ca9d
Ondřej Ezr <ezrik12@gmail.com>
2019-09-10 20:15:57 +0200

    Fixes #27820 - setting initialization init (#93)

--- app/models/setting/foreman_chef.rb
+++ app/models/setting/foreman_chef.rb
@@ -1,23 +1,13 @@
 class Setting::ForemanChef < Setting
-
-  def self.load_defaults
-    # Check the table exists
-    return unless super
-
-    self.transaction do
-      [
-          self.set('chef_interval', N_("Duration in minutes after servers reporting via Chef are classed as out of sync."), 60),
-          self.set('auto_deletion', N_("Enable the auto deletion of mapped objects in chef-server through foreman-proxy (currently node and client upon host deletion, client on host rebuild)"), true),
-          self.set('validation_bootstrap_method', N_("Use validation.pem or create client directly storing private key in Foreman DB)"), true),
-      ].each { |s| self.create! s.update(:category => "Setting::ForemanChef")}
-    end
-
-    true
-
+  def self.default_settings
+    [
+        self.set('chef_interval', N_("Duration in minutes after servers reporting via Chef are classed as out of sync."), 60),
+        self.set('auto_deletion', N_("Enable the auto deletion of mapped objects in chef-server through foreman-proxy (currently node and client upon host deletion, client on host rebuild)"), true),
+        self.set('validation_bootstrap_method', N_("Use validation.pem or create client directly storing private key in Foreman DB)"), true),
+    ]
   end
 
   def self.humanized_category
     _('Chef')
   end
-
 end
